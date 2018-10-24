90277f1e2c883e2b076a4938cfad6d4cb6cd097a
Axel <axel@sweetas.com>
2018-10-24 21:52:01 +0200

    configure local file to use for providing KPI

--- local_kpis_microservice/src/config.js
+++ local_kpis_microservice/src/config.js
@@ -2,7 +2,8 @@
 'use strict';
 
 const config = {
-    "PORT" : 3001
+    "PORT" : 3001,
+    "KPI_FILE" : "local_kpis.json"
 };
 
 module.exports = config;
--- local_kpis_microservice/src/main.js
+++ local_kpis_microservice/src/main.js
@@ -2,11 +2,13 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
 
 const config = require('./config');
 const constructRouter = require('./routes');
 
 const PORT = config.PORT;
+const KPI_FILE = config.KPI_FILE;
 
 const app = express();
 
@@ -17,6 +19,14 @@ app.use('/',routes);
 // fallback
   app.get('*',function (req, res) {
     res.sendStatus(404);
+});
+
+
+fs.readFile(KPI_FILE, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
 
 
