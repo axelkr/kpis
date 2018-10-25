f7c8fb00604187da8cf0283449da6f6777b71948
Axel <axel@sweetas.com>
2018-10-25 21:54:57 +0200

    check configuration for missing entries

--- local_kpis_microservice/src/config.js
+++ local_kpis_microservice/src/config.js
@@ -3,7 +3,28 @@
 
 const fs = require('fs');
 
-var rawConfig = fs.readFileSync('config.json', 'utf8');
-var config = JSON.parse(rawConfig);
+const configFile = 'config.json';
+const default_PORT = 3001;
+const default_KPI_FILE = "local_kpis.json";
 
+var config = {};
+
+if (fs.existsSync(configFile)) {
+    var rawConfig = fs.readFileSync(configFile, 'utf8');
+    config = JSON.parse(rawConfig);
+} else {
+    console.log("config file "+configFile+" not found. Using defaults instead.");
+}
+if (!config.hasOwnProperty('PORT')) {
+    console.log("no PORT configured. Use default "+default_PORT);
+    config.PORT = default_PORT;
+}
+if (!Number.isInteger(config.PORT)|| config.PORT < 1)  {
+    console.log("PORT not configured to a positive integer. Fall back to default "+default_PORT);
+    config.PORT = default_PORT;
+}
+if (!config.hasOwnProperty('KPI_FILE')) {
+    console.log("no KPI_FILE configured. Use default "+default_KPI_FILE);
+    config.KPI_FILE = default_KPI_FILE;
+}
 module.exports = config;
