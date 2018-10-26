267f3a08cf303e4164b1d17ea7a8dcb23a3a7634
Axel <axel@sweetas.com>
2018-10-26 22:18:00 +0200

    provide overview as well

--- visualization_app/src/kpis/views/SingleNumberWithoutDeadline.js
+++ visualization_app/src/kpis/views/SingleNumberWithoutDeadline.js
@@ -2,6 +2,7 @@
 'use strict';
 
 import React from 'react';
+import * as d3 from 'd3';
 
 import type KPI from '../records/KPI';
 
@@ -20,8 +21,31 @@ class SingleNumberWithoutDeadline extends React.Component<SingleNumberWithoutDea
   }
 
   render(){
-    return (<label>TBD</label>)
+    var currentValue = this.latestValue(this.props.KPI.measurements);
+    return (
+      <div>{currentValue}(goal {this.props.KPI.goal.target})</div>
+    )
+  }
+
+  latestValue(measurements) {
+    if (measurements.length == 0) {
+      return "-";
+    }
+    
+    var parseTime = d3.timeParse("%Y-%m-%d");
+    var latestValue = measurements[0].value;
+    var latestDate  = parseTime(measurements[0].date);
+    
+    measurements.forEach(function(element) {
+      var dateElement = parseTime(element.date);
+      if (dateElement > latestDate) {
+        latestDate = dateElement;
+        latestValue = element.value;
+      }
+    })
+    return parseFloat(latestValue);
   }
 }
 
 export default SingleNumberWithoutDeadline;
+  
