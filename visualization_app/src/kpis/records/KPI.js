cc110708bc2773c3a7ae6298d9776758ae796c38
Axel <axel@sweetas.com>
2018-10-26 18:04:50 +0200

    convert complete KPI

--- visualization_app/src/kpis/records/KPI.js
+++ visualization_app/src/kpis/records/KPI.js
@@ -6,20 +6,40 @@ import Immutable from 'immutable';
 declare class KPI {
   _id: string;
   name: string;
+  description:string;
+  type:string;
+  tags:array;
+  goal:any;
+  measurements:any;
 
   constructor(data: {
     _id: string;
     name: string;
+    description:string;
+    type:string;
+    tags:array;
+    goal:any;
+    measurements:any;
   }): void;
 
   set(key: '_id', value: string): KPI;
   set(key: 'name', value: string): KPI;
+  set(key: 'description', value: string): KPI;
+  set(key: 'type', value: string): KPI;
+  set(key: 'tags', value: array): KPI;
+  set(key: 'goal', value: any): KPI;
+  set(key: 'measurements', value: any): KPI;
 }
 
 // $FlowExpectedError: Intentional rebinding for flow.
 const KPI = Immutable.Record({
   _id: '',
   name: '',
+  description: '',
+  type:'',
+  tags:[],
+  goal:{},
+  measurements:{}
 });
 
 export default KPI;
