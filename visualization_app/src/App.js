1d543f7e5a5b16054ed4e540202844ebd1fca04b
Axel <axel@sweetas.com>
2018-10-26 07:33:26 +0200

    Simplify: remove separate routes and initial page

--- visualization_app/src/App.js
+++ visualization_app/src/App.js
@@ -2,20 +2,14 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import LandingPage from './LandingPage';
 import KPIApp from './kpis/views/KPIApp';
-
-
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App(props){
     return (
-      <Router>
         <div className="App">
-          <Route exact path="/" render={routingProps => (<LandingPage {...props}/>)}/>   
-          <Route exact path="/" render={routingProps => (<KPIApp {...props}/>)}/>     
+        <h2>KPIs</h2>
+          <KPIApp {...props}/> 
         </div>
-      </Router>
     );
   };
 
