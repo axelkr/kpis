bae863ac007fbcd12316c1e4fc6ac134b1efb2b3
Axel <axel@sweetas.com>
2018-10-27 09:08:39 +0200

    Add node for details of KPI

---
+++ visualization_app/src/kpis/views/OverlayKPIDetails.js
@@ -0,0 +1,19 @@
+// @flow
+'use strict';
+
+
+import React from 'react';
+
+import classnames from 'classnames';
+import '../../App.css';
+import '../KPI.css';
+
+class OverlayKPIDetails extends React.Component {
+  render(){    
+    return (
+        <div className="overlay">Details</div>
+    );
+  }
+}
+
+export default OverlayKPIDetails;
