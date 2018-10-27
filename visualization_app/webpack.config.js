a0d4aabd0c2276875a1b5ccad871ac3f37e0f89b
Axel <axel@sweetas.com>
2018-10-27 07:49:56 +0200

    configure in one place only

--- visualization_app/webpack.config.js
+++ visualization_app/webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
 module.exports = {
+  entry: {
+    root: './src/root.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
