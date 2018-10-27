3d0c6a453d89f747b0bd2e42445e1d3052ec8f58
Axel <axel@sweetas.com>
2018-10-27 14:56:27 +0200

    better naming

--- local_kpis_microservice/src/main.js
+++ local_kpis_microservice/src/main.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const config = require('./config');
 const constructRouter = require('./routes');
-const constructKPIFileWatcher = require('./kpiFileWatcher');
+const KPIFileWatcher = require('./kpiFileWatcher');
 const KPIStore = require('./kpiStore');
 
 const PORT = config.PORT;
@@ -14,7 +14,7 @@ const KPI_FILE = config.KPI_FILE;
 const app = express();
 
 const kpiStore = new KPIStore();
-const fileWatcher = constructKPIFileWatcher(KPI_FILE);
+const fileWatcher = KPIFileWatcher(KPI_FILE);
 fileWatcher.startWatching();
 fileWatcher.callSetOnUpdate(kpiStore);
 fileWatcher.updated();
