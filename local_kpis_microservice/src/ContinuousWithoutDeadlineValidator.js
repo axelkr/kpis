20552d538ea8e697ddbefeba1625035ae3d7da6b
Axel <axel@sweetas.com>
2018-10-28 09:54:23 +0100

    validate remaining properties

--- local_kpis_microservice/src/ContinuousWithoutDeadlineValidator.js
+++ local_kpis_microservice/src/ContinuousWithoutDeadlineValidator.js
@@ -14,8 +14,28 @@ class ContinuousWithoutDeadlineValidator {
       return false;
     }
 
+    if (!aKPI.measurements.every(isValidMeasurement)){
+      return false;
+    }
+
     return true;
   }
+}
+
+function isValidMeasurement(aMeasurement) {
+  if (aMeasurement === null || typeof aMeasurement !== 'object' || aMeasurement === undefined) {
+    return false;
+  }
+
+  if (!aMeasurement.hasOwnProperty('value')||!Number.isFinite(aMeasurement.value)) {
+    return false;
+  }
+
+  if (!aMeasurement.hasOwnProperty('date')||!(typeof aMeasurement.date === 'string' || aMeasurement.date instanceof String)) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = ContinuousWithoutDeadlineValidator;
--- local_kpis_microservice/src/CumulativeNumberOverYearValidator.js
+++ local_kpis_microservice/src/CumulativeNumberOverYearValidator.js
@@ -13,9 +13,30 @@ class CumulativeNumberOverYearValidator {
     if (!aKPI.goal.hasOwnProperty('target')||!Number.isFinite(aKPI.goal.target)) {
       return false;
     }
+    
+    if (!aKPI.measurements.every(isValidMeasurement)){
+      return false;
+    }
 
     return true;
   }
+}
+
+
+function isValidMeasurement(aMeasurement) {
+  if (aMeasurement === null || typeof aMeasurement !== 'object' || aMeasurement === undefined) {
+    return false;
+  }
+
+  if (!aMeasurement.hasOwnProperty('value')||!Number.isFinite(aMeasurement.value)) {
+    return false;
+  }
+
+  if (!aMeasurement.hasOwnProperty('date')||!(typeof aMeasurement.date === 'string' || aMeasurement.date instanceof String)) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = CumulativeNumberOverYearValidator;
