a62587c4db146ae2f35cf009c7b525b778bd8436
Axel <axel@sweetas.com>
2018-10-28 05:53:28 +0100

    ease implementation

--- local_kpis_microservice/spec/kpiStore.test.js
+++ local_kpis_microservice/spec/kpiStore.test.js
@@ -1,5 +1,14 @@
 const KPIStore = require('../src/KPIStore');
 
+function minimalKPI(_id,name,type) {
+  return {
+    '_id' : _id,
+    'name' : name,
+    'type' : type
+  }
+}
+
+
 test('read: accepts empty list of KPIs', () => {
   var kpis = {kpis:[]};
   var kpiStore = new KPIStore();
