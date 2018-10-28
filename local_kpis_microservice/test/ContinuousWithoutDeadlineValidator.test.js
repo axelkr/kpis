a3af1a441540f1732e7ab902cc69fef95574b148
Axel <axel@sweetas.com>
2018-10-28 09:40:28 +0100

    test validators as well

--- local_kpis_microservice/test/ContinuousWithoutDeadlineValidator.test.js
+++ local_kpis_microservice/test/ContinuousWithoutDeadlineValidator.test.js
@@ -28,4 +28,67 @@ describe('Validation of KPIs of type continuous_without_deadline', () => {
     var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
     expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).toBeTruthy();
   });
+
+  test('isValid: accepts empty list of measurements', () => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+    aKPI.measurements = [];
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).toBeTruthy();
+  });
+
+  test('isValid: accepts list of measurement with value and date', () => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+    aKPI.measurements = [{"date" : "2013-04-08" , "value" : 2}];
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).toBeTruthy();
+  });
+
+  test.each([{"value" : 2},{"date" : "2013-04-08"}])('isValid: rejects measurement without value or date', (incompleteMeasurement) => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+  
+    aKPI.measurements = [incompleteMeasurement];
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test.each([{"date" : "2013-04-08" , "value" : "2"},{"date" : 2013, "value" : 2}])('isValid: rejects measurement with incorrect types for value or date', (wrongTypeOfMeasurement) => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+  
+    aKPI.measurements = [wrongTypeOfMeasurement];
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+
+  test.each([2,undefined,[],'aString'])('isValid: rejects list of measurement with non-object types', (invalidObject) => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+
+    aKPI.measurements = [invalidObject];
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test('isValid: rejects KPI if field goal does not contain a field target', () => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+
+    delete aKPI.goal.target;
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+ test('isValid: accepts KPI if goal.target is a number', () => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+
+    aKPI.goal.target = 42;
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test.each([{},undefined,[],'aString'])('isValid: rejects KPI if goal.target is not a number', (objectNotANumber) => {
+    var aKPI = randomContinuousWithoutDeadlineKPI();
+
+    aKPI.goal.target = objectNotANumber;
+    var aContinuousWithoutDeadlineValidator = new ContinuousWithoutDeadlineValidator();
+    expect(aContinuousWithoutDeadlineValidator.isValid(aKPI)).not.toBeTruthy();
+  });
 });
--- local_kpis_microservice/test/CumulativeNumberOverYearValidator.test.js
+++ local_kpis_microservice/test/CumulativeNumberOverYearValidator.test.js
@@ -14,9 +14,9 @@ function randomCumulativeNumberOverYearKPI() {
   const goal = {"target" : 0};
 
   const measurements = [
-    {"date" : "15.10.18" , "value" : 13.40},
-    {"date" : "15.10.18" , "value" : 19.24},
-    {"date" : "04.10.18" , "value" : 12.17}
+    {"date" : "15.10.18" , "value" : 13.4},
+    {"date" : "13.10.18" , "value" : 1.24},
+    {"date" : "04.10.18" , "value" : 2.17}
   ];
 
   return minimalKPI(123,'name','cumulative_number_over_year',goal,measurements);
@@ -27,6 +27,69 @@ describe('Validation of KPIs of type cumulative_number_over_year', () => {
     var aKPI = randomCumulativeNumberOverYearKPI();
     var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
     expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).toBeTruthy();
+  });
+
+  test('isValid: accepts empty list of measurements', () => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+    aKPI.measurements = [];
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).toBeTruthy();
+  });
+
+  test('isValid: accepts list of measurement with value and date', () => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+    aKPI.measurements = [{"date" : "15.10.18" , "value" : 2}];
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).toBeTruthy();
+  });
+
+  test.each([{"value" : 2},{"date" : "15.10.18"}])('isValid: rejects measurement without value or date', (incompleteMeasurement) => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+  
+    aKPI.measurements = [incompleteMeasurement];
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test.each([{"date" : "15.10.18" , "value" : "2"},{"date" : 2013, "value" : 2}])('isValid: rejects measurement with incorrect types for value or date', (wrongTypeOfMeasurement) => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+  
+    aKPI.measurements = [wrongTypeOfMeasurement];
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+
+  test.each([2,undefined,[],'aString'])('isValid: rejects list of measurement with non-object types', (invalidObject) => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+
+    aKPI.measurements = [invalidObject];
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test('isValid: rejects KPI if field goal does not contain a field target', () => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+
+    delete aKPI.goal.target;
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+ test('isValid: accepts KPI if goal.target is a number', () => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+
+    aKPI.goal.target = 42;
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
+  });
+
+  test.each([{},undefined,[],'aString'])('isValid: rejects KPI if goal.target is not a number', (objectNotANumber) => {
+    var aKPI = randomCumulativeNumberOverYearKPI();
+
+    aKPI.goal.target = objectNotANumber;
+    var aCumulativeNumberOverYearValidator = new CumulativeNumberOverYearValidator();
+    expect(aCumulativeNumberOverYearValidator.isValid(aKPI)).not.toBeTruthy();
   });
 });
 
