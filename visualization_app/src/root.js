f1cce2948e1e40209be430b8643bbe716f07fb65
Axel <axel@sweetas.com>
2018-11-18 09:37:50 +0100

    remove service worker for time being

--- visualization_app/src/root.js
+++ visualization_app/src/root.js
@@ -3,8 +3,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AppContainer />, document.getElementById('root'));
-
-registerServiceWorker();
