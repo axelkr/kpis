eccafe9095a2e801e59f95daf8dfdf45bdfe2b6f
Axel <axel@sweetas.com>
2018-11-21 06:30:08 +0100

    provide additional test for dates

--- local_kpis_microservice/src/DataTypeValidator.js
+++ local_kpis_microservice/src/DataTypeValidator.js
@@ -5,6 +5,10 @@ class DataTypeValidator {
   isFloat(aValue:any) {
     return Number.isFinite(aValue);
   }
+
+  isDate(aValue:any) {
+    return (typeof aValue === 'string' || aValue instanceof String);
+  }
 }
 
 module.exports = DataTypeValidator;
--- local_kpis_microservice/test/DataTypeValidator.test.js
+++ local_kpis_microservice/test/DataTypeValidator.test.js
@@ -10,4 +10,16 @@ describe('Validation of data types', () => {
     var aDataTypeValidator = new DataTypeValidator();
     expect(aDataTypeValidator.isFloat(objectNotANumber)).not.toBeTruthy();
   });
+
+  test('isDate: accepts a date', () => {
+    var aDataTypeValidator = new DataTypeValidator();
+    expect(aDataTypeValidator.isDate("2018-02-18")).toBeTruthy();
+    expect(aDataTypeValidator.isDate("18.02.2018")).toBeTruthy();
+    expect(aDataTypeValidator.isDate("18.02.18")).toBeTruthy();
+  });
+
+  test.each([{},undefined,[],42])('isDate: rejects if value is not a date', (objectNotADate) => {
+    var aDataTypeValidator = new DataTypeValidator();
+    expect(aDataTypeValidator.isDate(objectNotADate)).not.toBeTruthy();
+  });
 });
