5ae70d73ef3925a7773a87bbb5c59a53024b69fd
Axel <axel@sweetas.com>
2018-11-21 06:00:57 +0100

    Visualize new KPI with sister KPI

--- visualization_app/src/kpis/visualizations/kpiTypeToComponent.js
+++ visualization_app/src/kpis/visualizations/kpiTypeToComponent.js
@@ -9,6 +9,8 @@ import type LoadObject from '../../utils/LoadObject';
 
 const SingleNumberWithoutDeadline = React.lazy(() => import('./SingleNumberWithoutDeadline'));
 const SingleNumberWithoutDeadlineDetails = React.lazy(() => import('./SingleNumberWithoutDeadlineDetails'));
+const SingleNumberWithDeadline = React.lazy(() => import('./SingleNumberWithDeadline'));
+const SingleNumberWithDeadlineDetails = React.lazy(() => import('./SingleNumberWithDeadlineDetails'));
 const CumulativeNumberPerYear = React.lazy(() => import('./CumulativeNumberPerYear'));
 const CumulativeNumberPerYearDetails = React.lazy(() => import('./CumulativeNumberPerYearDetails'));
 
@@ -17,6 +19,10 @@ const kpiTypeToComponent = {};
 kpiTypeToComponent.selectSummaryComponent = function(type:string,KPILo:LoadObject<KPI>) {
     if (type === "continuous_without_deadline") {
         return (<SingleNumberWithoutDeadline KPI={KPILo}/>)
+    }
+
+    if (type === "continuous_with_deadline") {
+        return (<SingleNumberWithDeadline KPI={KPILo}/>)
     }
 
     if (type === "cumulative_number_over_year") {
@@ -29,6 +35,10 @@ kpiTypeToComponent.selectSummaryComponent = function(type:string,KPILo:LoadObjec
 kpiTypeToComponent.selectDetailsComponent = function(type:string,KPILo:LoadObject<KPI>) {
     if (type === "continuous_without_deadline") {
         return (<SingleNumberWithoutDeadlineDetails KPI={KPILo}/>)
+    }
+
+    if (type === "continuous_with_deadline") {
+        return (<SingleNumberWithDeadlineDetails KPI={KPILo}/>)
     }
     
     if (type === "cumulative_number_over_year") {
