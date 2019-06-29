c8ecbaaa0b5d40688de7d9aa311c07c7900f5add
Axel <axel@sweetas.com>
2019-06-29 22:39:14 +0200

    proper reading

--- visualization_app/src/kpis/visualizations/SprintBurndownDetails.js
+++ visualization_app/src/kpis/visualizations/SprintBurndownDetails.js
@@ -2,6 +2,7 @@
 'use strict';
 
 import React from 'react';
+import moment from 'moment';
 
 import type KPI from '../records/KPI';
 import type LoadObject from '../../utils/LoadObject';
@@ -22,8 +23,25 @@ class SprintBurndownDetails extends React.Component<SprintBurndownDetailsProps, 
       return null;
     }
     var KPI = this.props.KPI.getValueEnforcing();
+    var sprints = KPI.measurements;
+    sprints = convertToProperJSObjects(sprints);
+    sprints.sort((a,b)=>a.startedOn-b.startedOn);
     return null;
   }
+}
+
+function convertToProperJSObjects(sprints) {
+  const dateTimeFormat = 'DD.MM.YYYY';
+  sprints.map((aSprint)=>{
+    aSprint.startedOn = moment(aSprint.startedOn,dateTimeFormat);
+    aSprint.endsOn = moment(aSprint.endsOn,dateTimeFormat);
+    aSprint.measurements.map((aMeasurement)=>{
+      aMeasurement.date = moment(aMeasurement.date,dateTimeFormat);
+      return aMeasurement;
+    })
+    return aSprint;
+  })
+  return sprints;
 }
 
 export default SprintBurndownDetails;
