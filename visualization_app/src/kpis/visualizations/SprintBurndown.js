88f6347c8d8c06cc1b036fe9b1ab97f8e4140121
Axel <axel@sweetas.com>
2019-06-29 22:00:31 +0200

    simplify

--- visualization_app/src/kpis/visualizations/SprintBurndown.js
+++ visualization_app/src/kpis/visualizations/SprintBurndown.js
@@ -2,7 +2,6 @@
 'use strict';
 
 import React from 'react';
-import * as d3 from 'd3';
 
 import type KPI from '../records/KPI';
 import type LoadObject from '../../utils/LoadObject';
@@ -18,38 +17,19 @@ type SprintBurndownState = {
 };
 
 class SprintBurndown extends React.Component<SprintBurndownProps,SprintBurndownState> {
-  constructor(props:SprintBurndownProps){
-    super(props);
-  }
-
   render(){
     if (!this.props.KPI.hasValue()) {
       return null;
     }
     var KPI = this.props.KPI.getValueEnforcing();
-    var currentValue = this.cumulativeValueThisYear(KPI.measurements);
+    /*var currentValue = this.cumulativeValueThisYear(KPI.measurements);
     currentValue = Math.round(100*currentValue)/100;
     return (
       <div>{currentValue} (goal {KPI.goal.target})</div>
+    )*/
+    return (
+      <div>SprintBurndown</div>
     )
-  }
-
-  cumulativeValueThisYear(measurements) {
-    if (measurements.length == 0) {
-      return "-";
-    }
-    
-    var sumOfValuesThisYear = 0;
-    var parseTime = d3.timeParse("%d.%m.%y");
-    const currentYear = new Date().getFullYear();
-    
-    measurements.forEach(function(element) {
-      var yearOfMeasurement = parseTime(element.date).getFullYear();
-      if (yearOfMeasurement == currentYear) {
-        sumOfValuesThisYear = sumOfValuesThisYear + parseFloat(element.value);
-      }
-    })
-    return sumOfValuesThisYear;
   }
 }
 
