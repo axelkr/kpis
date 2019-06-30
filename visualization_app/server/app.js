1d71087b79f52c4acdb69e69102bc9f22eeedf29
Axel <axel@sweetas.com>
2019-06-30 09:55:00 +0200

    first shot at removing warnings of incorrect mime type

--- visualization_app/server/app.js
+++ visualization_app/server/app.js
@@ -2,12 +2,16 @@
 'use strict';
 
 const express = require('express');
-const request = require('request');
 const config = require('./config');
 
 const PORT = config.PORT;
 
 const app = express();
+
+app.use('*.js', (req, res, next) => {
+  res.set('Content-Type', 'text/javascript')
+  next();
+})
 
 app.use('/', express.static('.'));
 app.use(express.static('dist'));
