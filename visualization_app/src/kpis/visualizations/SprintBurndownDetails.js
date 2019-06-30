68b32d7ec327c961f8db651c3fd16dbbd5040f93
Axel <axel@sweetas.com>
2019-06-30 09:55:28 +0200

    normalize data before display

--- visualization_app/src/kpis/visualizations/SprintBurndownDetails.js
+++ visualization_app/src/kpis/visualizations/SprintBurndownDetails.js
@@ -25,21 +25,49 @@ class SprintBurndownDetails extends React.Component<SprintBurndownDetailsProps, 
     var KPI = this.props.KPI.getValueEnforcing();
     var sprints = KPI.measurements;
     sprints = convertToProperJSObjects(sprints);
+    sprints = normalizeMeasurements(sprints);
     sprints.sort((a,b)=>a.startedOn-b.startedOn);
     return null;
   }
 }
 
+function normalizeMeasurements(sprints) {
+  sprints.forEach(value => {
+    value.measurements = value.measurements.sort((a,b)=>a.date-b.date);
+    value.measurements = fillMissingUserStoryPoints(value.measurements, value.startedOn);
+  });
+  return sprints;
+}
+
+function fillMissingUserStoryPoints(measurements,firstDayOfSprint) {
+  var nextIndexToCheck = measurements.length-2;
+  var nextDay = measurements[measurements.length-1];
+  while(nextIndexToCheck>=0){
+    var dayToCheck = measurements[nextIndexToCheck];
+    var insertDayBeforeNextDay = moment.duration(new moment(nextDay.date).diff(new moment(dayToCheck.date))).asDays()>1;
+    if (insertDayBeforeNextDay) {
+      var additionalDay = {
+        "date" : new moment(nextDay.date).add(-1, 'days').toDate(),
+        "numberStoryPoints" : nextDay.numberStoryPoints
+      };
+      measurements.splice(nextIndexToCheck+1, 0, additionalDay);
+      nextDay = additionalDay;
+    } else {
+      nextDay = dayToCheck;
+      nextIndexToCheck = nextIndexToCheck-1;
+    }
+  }
+  return measurements;
+}
+
 function convertToProperJSObjects(sprints) {
   const dateTimeFormat = 'DD.MM.YYYY';
-  sprints.map((aSprint)=>{
-    aSprint.startedOn = moment(aSprint.startedOn,dateTimeFormat);
-    aSprint.endsOn = moment(aSprint.endsOn,dateTimeFormat);
-    aSprint.measurements.map((aMeasurement)=>{
-      aMeasurement.date = moment(aMeasurement.date,dateTimeFormat);
-      return aMeasurement;
+  sprints.forEach((aSprint)=>{
+    aSprint.startedOn = moment(aSprint.startedOn,dateTimeFormat).toDate();
+    aSprint.endsOn = moment(aSprint.endsOn,dateTimeFormat).toDate();
+    aSprint.measurements.forEach((aMeasurement)=>{
+      aMeasurement.date = moment(aMeasurement.date,dateTimeFormat).toDate();
     })
-    return aSprint;
   })
   return sprints;
 }
