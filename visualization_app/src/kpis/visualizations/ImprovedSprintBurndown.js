56e141953382daa5d0f84858bb9bc2ce0eb092b0
Axel <axel@sweetas.com>
2019-06-30 10:58:51 +0200

    Finally, draw sprints

--- visualization_app/src/kpis/visualizations/ImprovedSprintBurndown.js
+++ visualization_app/src/kpis/visualizations/ImprovedSprintBurndown.js
@@ -4,29 +4,47 @@
 import React from 'react';
 import moment from 'moment';
 
-import '../../App.css';
+import {
+    LineChart, Line, ReferenceLine
+} from 'recharts';
 
 function ImprovedSprintBurndown(props) {
-  var sprints = props.sprints;
-  var inRechartsLayout = convertToRecharts(sprints);
-  var sprintKeys = sprints.map((aSprint)=>toStableKey(aSprint));
-  const currentSprint = sprintKeys.pop();
-  return (
-    <div>Improved SprintBurndown</div>
-  );
+    var sprints = props.sprints;
+    var inRechartsLayout = convertToRecharts(sprints);
+    var sprintKeys = sprints.map((aSprint) => toStableKey(aSprint));
+    const currentSprint = sprintKeys.pop();
+
+    const linesOfpreviousSprints = sprintKeys.map((aSprint) =>
+        <Line key={aSprint} stroke="#880000" type="monotone" dataKey={aSprint} dot={false} isAnimationActive={false} />
+    );
+
+    return (
+        <LineChart
+            width={960}
+            height={500}
+            data={inRechartsLayout}
+            margin={{
+                top: 5, right: 30, left: 20, bottom: 5,
+            }}
+        >
+            {linesOfpreviousSprints}
+            <ReferenceLine y={0} stroke="b" stroke="white"/>
+            <Line key={currentSprint} type="monotone" dataKey={currentSprint} stroke="#ff0000" strokeWidth="2" dot={false} isAnimationActive={false} />
+        </LineChart>
+    );
 }
 
 function convertToRecharts(sprints) {
-    var inRecharts = initialRechartsSetup(sprints[sprints.length-1]);
-    sprints.forEach((aSprint)=>{
-        extendRechartsSetup(aSprint,inRecharts);
+    var inRecharts = initialRechartsSetup(sprints[sprints.length - 1]);
+    sprints.forEach((aSprint) => {
+        extendRechartsSetup(aSprint, inRecharts);
     })
     return inRecharts;
 }
 
-function extendRechartsSetup(aSprint,inRecharts) {
+function extendRechartsSetup(aSprint, inRecharts) {
     const sprintKey = toStableKey(aSprint);
-    aSprint.measurements.forEach((value)=> {
+    aSprint.measurements.forEach((value) => {
         var indexInRecharts = moment.duration(new moment(value.date).diff(new moment(aSprint.startedOn))).asDays();
         inRecharts[indexInRecharts][sprintKey] = value.numberStoryPoints;
     });
@@ -36,12 +54,12 @@ function initialRechartsSetup(lastSprint) {
     var inRecharts = [];
     var nextEntry = lastSprint.startedOn;
     var finalEntry = lastSprint.endsOn;
-    while( nextEntry <= finalEntry) {
+    while (nextEntry <= finalEntry) {
         var anEntry = {
-            'date' : nextEntry
+            'date': nextEntry
         }
         inRecharts.push(anEntry);
-        nextEntry = new moment(nextEntry).add(1,'days').toDate();
+        nextEntry = new moment(nextEntry).add(1, 'days').toDate();
     }
     return inRecharts;
 }
